d98276f4960257bd8c4a8ebd4f6bcb14b294bbc9
Yun <duwls0349@naver.com>
2022-10-24 00:10:56 +0900

    Feat: EndPoint 생성

---
+++ GetPhoto/EndPoint.swift
@@ -0,0 +1,15 @@
+//
+//  EndPoint.swift
+//  GetPhoto
+//
+//  Created by 윤여진 on 2022/10/24.
+//
+
+import Foundation
+
+enum unsplashURL {
+    
+    static let baseURL = "https://api.unsplash.com"
+    static let randomPhotoURL = "https://api.unsplash.com/search/random?query="
+
+}
