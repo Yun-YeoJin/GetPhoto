1869169404e1c1213440c7b850a363ac11729af1
Yun <duwls0349@naver.com>
2022-10-23 23:14:29 +0900

    Initial Commit

---
+++ GetPhoto/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GetPhoto
+//
+//  Created by 윤여진 on 2022/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
