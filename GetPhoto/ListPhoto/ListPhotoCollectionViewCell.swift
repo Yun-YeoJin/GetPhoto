2767c3652beee710aec8e5138cc8cb2c565b7093
Yun <duwls0349@naver.com>
2022-10-24 22:44:45 +0900

    Feat: ListPhotoCell UI 설정

--- GetPhoto/ListPhoto/ListPhotoCollectionViewCell.swift
+++ GetPhoto/ListPhoto/ListPhotoCollectionViewCell.swift
@@ -5,4 +5,50 @@
 //  Created by 윤여진 on 2022/10/24.
 //
 
-import Foundation
+import UIKit
+
+import Then
+import SnapKit
+
+class ListPhotoCollectionViewCell: UICollectionViewCell {
+    
+    let photoImageView = UIImageView().then {
+        $0.contentMode = .scaleAspectFit
+        $0.backgroundColor = .clear
+    }
+    
+    let likesLabel = UILabel().then {
+        $0.textColor = .label
+        $0.font = .boldSystemFont(ofSize: 15)
+        $0.textAlignment = .left
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+        
+        configureUI()
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configureUI() {
+        
+        contentView.addSubview(photoImageView)
+        contentView.addSubview(likesLabel)
+        
+        photoImageView.snp.makeConstraints { make in
+            make.top.leading.bottom.equalToSuperview().inset(8)
+            make.width.height.equalTo(50)
+        }
+        
+        likesLabel.snp.makeConstraints { make in
+            make.top.trailing.equalToSuperview().inset(8)
+            make.leading.equalTo(photoImageView.snp.trailing).offset(8)
+        }
+        
+    }
+}
+
