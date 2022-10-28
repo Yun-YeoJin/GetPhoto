b24e716ddbf5be83fd4b6589b4d25854172fea29
Yun <duwls0349@naver.com>
2022-10-28 17:10:13 +0900

    Feat: searchPhoto APIService 생성

--- GetPhoto/Network/APIService.swift
+++ GetPhoto/Network/APIService.swift
@@ -42,5 +42,19 @@ class APIService {
 
     }
     
+    static func searchPhoto(query: String, completion: @escaping (SearchPhoto?, Int?, Error?) -> Void) {
+        let url = "\(EndPoint.searchURL)\(query)"
+        let header: HTTPHeaders = ["Authorization": APIKey.unsplashKey]
+        
+        AF.request(url, method: .get, headers: header).responseDecodable(of: SearchPhoto.self) { response in
+            let statusCode = response.response?.statusCode
+            
+            switch response.result {
+            case .success(let value): completion(value, statusCode, nil)
+            case .failure(let error): completion(nil, statusCode, error)
+            }
+        }
+    }
+    
     
 }
