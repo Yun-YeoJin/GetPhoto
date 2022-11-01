83db87fa71bae98d8e192f37334d5c423f2e796b
Yun <duwls0349@naver.com>
2022-11-01 17:45:02 +0900

    Add: SearchPhoto ViewModel에 addFolder func 추가

--- GetPhoto/SearchPhoto/ViewModel/SearchPhotoViewModel.swift
+++ GetPhoto/SearchPhoto/ViewModel/SearchPhotoViewModel.swift
@@ -15,6 +15,8 @@ class SearchPhotoViewModel {
     
     var photoList = BehaviorSubject(value: SearchPhoto(total: 0, totalPages: 0, results: []))
     
+    let repository = PhotoFolderRepository()
+    
 }
 
 extension SearchPhotoViewModel {
@@ -27,6 +29,30 @@ extension SearchPhotoViewModel {
             switch result {
             case .success(let photo): self.photoList.onNext(photo)
             case .failure(let error): print(error.errorDescription!)
+            }
+        }
+    }
+    
+    func addFolder(item: SearchResult, text: String) {
+        
+        if repository.fetchFolderFilter(text: text).isEmpty { //PhotoListTable이 비어있으면
+            
+            let task = PhotoFolder(folderName: text)
+            task.photoDetail.append(Photo(photoID: item.id, photoURL: item.links.downloadLocation, photoDescription: item.resultDescription ?? text))
+            do {
+                try repository.addFolder(item: task)
+            } catch {
+                print("error")
+            }
+            
+        } else { //PhotoListTable이 안비어있으면
+            
+            guard let folder = repository.fetchFolderFilter(text: text).first else {return}
+            let item = Photo(photoID: item.id, photoURL: item.links.downloadLocation, photoDescription: item.resultDescription ?? text)
+            do {
+                try repository.appendPhoto(folder: folder, item: item)
+            } catch {
+                print("error")
             }
         }
     }
